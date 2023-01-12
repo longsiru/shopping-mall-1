39ad6b5b6ec023604bb73c8830b91ecde590b636
longsiru <sirulong89@gmail.com>
2023-01-12 15:54:30 +0900

    up

---
+++ myshop/src/main/java/com/myshop/dto/ItemDto.java
@@ -0,0 +1,30 @@
+package com.myshop.dto;
+
+import java.time.LocalDateTime;
+
+import com.myshop.constant.ItemSellStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class ItemDto {
+	
+	private long id; //상품코드
+	
+	private String itemNm;//상품명
+	
+	private int price;//가격
+	
+	private int stockNumber;//재고수량
+	
+	private String itemDetail;// 상품 상세성명
+	
+	private ItemSellStatus itemSellStatus;//상품 판매상태
+	
+	private LocalDateTime regTime;//등록시간
+	
+	private LocalDateTime updateTime;//수정시간
+}
